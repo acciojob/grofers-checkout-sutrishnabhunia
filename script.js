0448c324aca347b0092e285e91bf921e558e76c4
Sutrishna Bhunia <134145442+sutrishnabhunia@users.noreply.github.com>
2023-09-03 20:59:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
+ function calculateTotalPrice() {
+      const priceElements = document.querySelectorAll('.price');
+      let total = 0;
 
-const getSum = () => {
-//Add your code here
-  
-};
+      priceElements.forEach(priceElement => {
+        total += parseFloat(priceElement.textContent);
+      });
 
-getSumBtn.addEventListener("click", getSum);
+      return total.toFixed(2);
+    }
 
+    function updateTotalRow() {
+      const totalRow = document.querySelector('#grocery-table tfoot');
+      if (totalRow) {
+        totalRow.remove();
+      }
+
+      const table = document.getElementById('grocery-table');
+      const newRow = table.createTFoot().insertRow();
+      const totalCell = newRow.insertCell();
+      totalCell.setAttribute('colspan', '2');
+      totalCell.textContent = `Total Price: $${calculateTotalPrice()}`;
+    }
+
+    // Call the updateTotalRow function initially and whenever prices change
+    updateTotalRow();
